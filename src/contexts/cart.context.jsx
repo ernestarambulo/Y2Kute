2d2028dbd59ceb9e2ca2fdc1f92ab4f031ec3587
ernestarambulo <ernest.arambulo@gmail.com>
2022-05-27 19:50:50 -0400

    fixing bug that removes all items instead of just one cartItem.

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -23,7 +23,7 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
     );
     // check if quantity is equal to 1, if it is remove that item from cart
     if(existingCartItem.quantity === 1) {
-        return cartItems.filter(cartItem => cartItem.id ===! cartItemToRemove.id);
+        return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
     }
     // return back cartitems, with matching cart item with reduced quantity.
     return cartItems.map((cartItem) =>
@@ -31,22 +31,21 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
         ? {...cartItem, quantity: cartItem.quantity - 1 }
         : cartItem
         );
-}
+};
 
-export const clearCartItem = (cartItems, cartItemToClear) => {
-        return cartItems.filter((cartItem) => cartItem.id ===! cartItemToClear.id);
-}
+    const clearCartItem = (cartItems, cartItemToClear) =>
+        cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
 
 
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsOpen: () => {},
+    setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
     removeItemFromCart: () => {},
     clearItemFromCart: () => {},
     cartCount: 0,
-    cartTotal: 0
+    cartTotal: 0,
 });
 
 export const CartProvider = ({ children }) => {
@@ -56,18 +55,20 @@ export const CartProvider = ({ children }) => {
     const [cartTotal, setCartTotal] = useState(0);
 
     useEffect(() => {
+        const newCartCount = cartItems.reduce(
+            (total, cartItem) => total + cartItem.quantity,
+            0
+        );
+        setCartCount(newCartCount)
+    }, [cartItems]);
+
+    useEffect(() => {
         const newCartTotal = cartItems.reduce(
             (total, cartItem) => total + cartItem.quantity * cartItem.price,
             0
         );
         setCartTotal(newCartTotal);
-    }, [cartItems])
-
-
-    useEffect(() => {
-        const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
-        setCartCount(newCartCount)
-    }, [cartItems])
+    }, [cartItems]);
 
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
