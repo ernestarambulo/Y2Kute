078a7df2b22e97a211c76f8be661d02229557a14
ernestarambulo <ernest.arambulo@gmail.com>
2022-05-27 15:37:43 -0400

    created checkout page

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,14 +1,35 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { CartContext } from '../../contexts/cart.context';
+
 import Button from '../button/button'
+import CartItem from '../cart-item/cart-item';
 
 import './cart-dropdown.scss'
 
 const CartDropDown = () => {
-    return(
-        <div className='cart-dropdown-container'>
-            <div className='cart-items' />
-            <Button>GO TO CHECKOUT</Button>
-        </div>
-    );
-}
+    const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
 
-export default CartDropDown;
+    const goToCheckOutHandler = () => {
+        navigate('/checkout')
+    }
+
+    return (
+    <div className='cart-dropdown-container'>
+        <div className='cart-items'>
+        {cartItems.length ? (
+            cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+            ))
+        ) : (
+            <span className='empty-message'>Your cart is empty</span>
+        )}
+        </div>
+        <Button onClick={goToCheckOutHandler}>GO TO CHECKOUT</Button>
+    </div>
+    );
+};
+
+    export default CartDropDown;
