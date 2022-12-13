4d03b1fd382e328148f2e0d7ff259dac54320c5b
ernestarambulo <ernest.arambulo@gmail.com>
2022-12-13 04:44:45 -0500

    latest file update to ecommerce site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import {Routes, Route } from 'react-router-dom'
+import logo from './logo.svg';
+import './App.css';
 
-import Home from './routes/home/home'
-import Navigation from './routes/navigation/navigation';
-import Authentication from './routes/authentication/authentication';
-import Shop from './routes/shop/shop.component'
-import CheckOut from './routes/checkout/checkout';
-
-const App = () => {
+function App() {
   return (
-    <Routes>
-      <Route path= '/' element={<Navigation/>}>
-        <Route index element ={<Home />}/>
-        <Route path='shop/*' element ={<Shop />}/>
-        <Route path='auth' element ={<Authentication />}/>
-        <Route path='checkout' element = {<CheckOut/>}/>
-      </Route>
-    </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
-
-export default App
+export default App;
